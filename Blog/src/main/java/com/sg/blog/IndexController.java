d7aa1fb007155af6f61a1f258e06f1791ec98b21
jcazub <jcazub@gmail.com>
2018-06-07 02:54:09 -0400

    added IndexController

---
+++ Blog/src/main/java/com/sg/blog/IndexController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.blog;
+
+import com.sg.blog.service.BlogService;
+import javax.inject.Inject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Jesse
+ */
+@Controller
+public class IndexController {
+    
+    BlogService blogService;
+    
+    @Inject
+    public IndexController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String refresh(Model model) {
+        model.addAttribute("posts", blogService.getAllBlogs());
+        return "index";
+    }
+
+    
+}
